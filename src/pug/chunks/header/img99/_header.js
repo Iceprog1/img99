170a98d1e693efc1f06483cbde00ff0cc0950eb3
derkaz <derkaz72@gmail.com>
2023-05-28 12:08:07 +0300

    Добавил функцию перемещения элемента

--- src/pug/chunks/header/img99/_header.js
+++ src/pug/chunks/header/img99/_header.js
@@ -1,3 +1,4 @@
+import * as functions from '../../../js/functions.js';
 //function toggleClass(toggleElement) {
 //	document.querySelector(toggleElement).classList.toggle('active');
 //};
@@ -6,13 +7,31 @@
 
 //Поучаем элемент dropdown
 let dropdown = document.querySelectorAll('.dropdown');
-for (let i = 0; i < dropdown.length; i++){
-	dropdown[i].onclick = () => {
-		dropdown[i].parentElement.classList.toggle('active');
-	}
+for (let i = 0; i < dropdown.length; i++) {
+  dropdown[i].onclick = () => {
+    dropdown[i].parentElement.classList.toggle('active');
+    dropdown[i].firstElementChild.classList.toggle('rotate');
+  };
 }
 
+//let mobdropdown = document.querySelectorAll('.menuB');
+//for (let i = 0; i < mobdropdown.length; i++){
+//	mobdropdown[i].onclick = () => {
+//		document.querySelector('.header-mobmenu').classList.toggle('active');
+//	}
+//}
 
+functions.mouseLeave('header-right_lang');
+
+functions.addRemoveClass('menuB-line');
+
+functions.movingOnOff('-header-nav', 'header-right_lang','+header-right', 1140);
+functions.movingOnOff('header-nav', 'header-right', 'header-nav', 1140);
+
+//window.addEventListener('resize', (e) => {
+//  functions.movingOnOff('header-nav', 'header-right_lang', 1140, true);
+//  functions.movingOnOff('header-nav', 'header-right', 1140);
+//});
 //document.querySelector('.dropdown').onclick = () => {
 //	let en = document.querySelector('.header-right_lang__dropdown').classList.toggle('active');
 //}
--- src/pug/js/functions.js
+++ src/pug/js/functions.js
@@ -7,12 +7,68 @@ export function testWebP(callback) {
 	webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
 }
 
-testWebP(function (support) {
-
-	if (support == true) {
-		document.querySelector('body').classList.add('webp');
-	} else {
-		document.querySelector('body').classList.add('no-webp');
+//Удаление класса при отведении мыши у элемента
+export function mouseLeave (element, removeClass = 'active') {
+	let el = document.querySelectorAll(`.${element}`);
+	for (let i = 0; i < el.length; i++) {
+		el[i].onmouseleave = () => {
+			el[i].classList.remove(removeClass);
+		}
 	}
-});
+}
+
+//Добавление и удаление класса у элемента
+export function addRemoveClass (element, addRemove = 'openclose'){
+	let add = document.querySelector(`.${element}`);
+	add.onclick = () => {
+		add.classList.toggle(addRemove);
+	}
+}
+
+//Перемещает элемент на мобилке и возвращает обратно на десктоп версии
+export function movingOnOff(el, movElem, backEL, width) {
+	// Удаляет первый символ строки у параметра el
+	let elClass = el;
+	if(elClass[0] == '+' || elClass[0] ==  '-'){
+		elClass = el.substring(1);
+	}
+
+	// Удаляет первый символ строки у параметра backEL
+	let backElementClass = backEL;
+	if(backElementClass[0] == '+' || backElementClass[0] ==  '-'){
+		backElementClass = backEL.substring(1);
+	}
+	
+	// Получаем элементы со страницы
+	let element = document.querySelector(`.${elClass}`);
+	let movingElement = document.querySelector(`.${movElem}`);
+	let backElement = document.querySelector(`.${backElementClass}`);
+
+	if(window.innerWidth <= width){
+		if(el[0] == '-'){
+			element.prepend(movingElement);
+		}
+		else{
+			element.append(movingElement);
+		}
+	}
+	window.addEventListener("resize", (e) => {
+		if(window.innerWidth <= width){
+			if(el[0] == '-'){
+				element.prepend(movingElement);
+			}
+			else{
+				element.append(movingElement);
+			}
+		}
+		else{
+			if(backEL[0] == '+') {
+				backElement.prepend(movingElement);
+			}else{
+				backElement.parentNode.insertBefore(movingElement, backElement.nextSibling);
+			}
+		}
+	});
+}
+
 
