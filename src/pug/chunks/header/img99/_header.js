f4e9b186cec92213a84220b59ffe0e1f066bc418
derkaz <derkaz72@gmail.com>
2023-05-31 00:13:13 +0300

    Сделал Header пк и мобильную версию

--- src/pug/chunks/header/img99/_header.js
+++ src/pug/chunks/header/img99/_header.js
@@ -1,4 +1,4 @@
-import * as functions from '../../../js/functions.js';
+import * as functions from "../../../js/functions.js";
 //function toggleClass(toggleElement) {
 //	document.querySelector(toggleElement).classList.toggle('active');
 //};
@@ -6,32 +6,32 @@ import * as functions from '../../../js/functions.js';
 //document.querySelector('.close').onclick = () => {toggleClass('.header-nav') };
 
 //Поучаем элемент dropdown
-let dropdown = document.querySelectorAll('.dropdown');
-for (let i = 0; i < dropdown.length; i++) {
-  dropdown[i].onclick = () => {
-    dropdown[i].parentElement.classList.toggle('active');
-    dropdown[i].firstElementChild.classList.toggle('rotate');
-  };
-}
-
-//let mobdropdown = document.querySelectorAll('.menuB');
-//for (let i = 0; i < mobdropdown.length; i++){
-//	mobdropdown[i].onclick = () => {
-//		document.querySelector('.header-mobmenu').classList.toggle('active');
-//	}
+//let dropdown = document.querySelectorAll(".dropdownbtn");
+//let drop = document.querySelector(".accordion-list");
+//for (let i = 0; i < dropdown.length; i++) {
+//	dropdown[i].onclick = () => {
+//		//dropdown[i].parentElement.classList.toggle("active");
+//		dropdown[i].firstElementChild.classList.toggle("rotate");
+//		drop.classList.toggle("active");
+//	};
 //}
 
-functions.mouseLeave('header-right_lang');
 
-functions.addRemoveClass('menuB-line');
+functions.mouseLeave('.dropdown');
 
-functions.movingOnOff('-header-nav', 'header-right_lang','+header-right', 1140);
-functions.movingOnOff('header-nav', 'header-right', 'header-nav', 1140);
+functions.movingElement('.header-right', '.header-nav', '.header-nav', '1140');
 
-//window.addEventListener('resize', (e) => {
-//  functions.movingOnOff('header-nav', 'header-right_lang', 1140, true);
-//  functions.movingOnOff('header-nav', 'header-right', 1140);
-//});
+functions.addRemoveClass('.menubtn', '.header-nav');
+
+functions.addRemoveClass('.dropdownbtn', '.dropdown');
+
+
+// 3 параметр должен вставть после выбранного элемента - внутрь элемента (в начало или клнец)
+// +3.1 .header-nav - если передаем без знака вначале или в конце становится после этого элемента
+// 3.2 +.header-nav - если в начале есть плюс падает внутрь элемента в конец
+// 3.2 -.header-nav - если в начале есть минус падает внутрь элемента в начало
+functions.movingElement('.header-right_lang', '-.header-nav', '-.header-right', '1140');
+
 //document.querySelector('.dropdown').onclick = () => {
 //	let en = document.querySelector('.header-right_lang__dropdown').classList.toggle('active');
 //}
--- src/pug/js/functions.js
+++ src/pug/js/functions.js
@@ -1,74 +1,141 @@
-export function testWebP(callback) {
+//ширины экранов
+let sm = "540px";
+let md = "720px";
+let lg = "960px";
+let xl = "1140px";
 
+export function testWebP(callback) {
 	var webP = new Image();
 	webP.onload = webP.onerror = function () {
 		callback(webP.height == 2);
 	};
-	webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+	webP.src =
+		"data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
 }
 
 //Удаление класса при отведении мыши у элемента
-export function mouseLeave (element, removeClass = 'active') {
-	let el = document.querySelectorAll(`.${element}`);
+export function mouseLeave(element, removeClass = "active") {
+	let el = document.querySelectorAll(element);
+	if (removeClass[0] == '.') {
+		removeClass = removeClass.slice(1);
+	}
 	for (let i = 0; i < el.length; i++) {
 		el[i].onmouseleave = () => {
 			el[i].classList.remove(removeClass);
-		}
+		};
 	}
 }
 
-//Добавление и удаление класса у элемента
-export function addRemoveClass (element, addRemove = 'openclose'){
-	let add = document.querySelector(`.${element}`);
+
+//Добавление и удаление класса у основго и выбранного элемента
+export function addRemoveClass(element, nextElem = '', addRemove = "active") {
+	let add = document.querySelector(element);
+	let nextEl = document.querySelector(nextElem);
+	if (addRemove[0] == '.') {
+		addRemove = addRemove.slice(1);
+	}
 	add.onclick = () => {
 		add.classList.toggle(addRemove);
-	}
+		nextEl.classList.toggle(addRemove);
+	};
 }
 
-//Перемещает элемент на мобилке и возвращает обратно на десктоп версии
-export function movingOnOff(el, movElem, backEL, width) {
-	// Удаляет первый символ строки у параметра el
-	let elClass = el;
-	if(elClass[0] == '+' || elClass[0] ==  '-'){
-		elClass = el.substring(1);
-	}
+// Я хочу чтобы при уменьшении экрана элемент перемещался в нижнюю часть другого элемента, а при увеличении экрана чтобы данный элемент возвращался в исходное состояние
+// 1.
+//		+1.1 Переместить выбранный элемент внутрь другого элемента в его нижнюю часть
+// 		+1.2 Также мне нужна возможность при необходимости перемещать данный элемент в верхнюю часть внутри другого элемента
+//		1.3 Также нужна возможность переместить данный элемент после выбранного элемента
+// 2.
+//		2.1 При увеличении экрана вернуть элемент в исходное состояние
 
-	// Удаляет первый символ строки у параметра backEL
-	let backElementClass = backEL;
-	if(backElementClass[0] == '+' || backElementClass[0] ==  '-'){
-		backElementClass = backEL.substring(1);
-	}
-	
-	// Получаем элементы со страницы
-	let element = document.querySelector(`.${elClass}`);
-	let movingElement = document.querySelector(`.${movElem}`);
-	let backElement = document.querySelector(`.${backElementClass}`);
+//functions.moving('.header-right', '-.header-nav', '.span', '1140');
 
-	if(window.innerWidth <= width){
-		if(el[0] == '-'){
-			element.prepend(movingElement);
-		}
-		else{
-			element.append(movingElement);
-		}
+export function movingElement(movEl, whereEl, backEl, width) {
+	//----------------------------------------------------------------------------
+	// Удаление первого символа у строки
+	// Переменной whereElRemove присваиваем значение параметра whereEl
+	let whereElRemove = whereEl; // whereElRemove = -.header-nav
+	// Пишем условие если у переменной whereElRemove первый символ равен -
+	if (whereElRemove[0] == "-") {
+		// то удаляем первый символ и присваиваем переменной whereElRemove значение без минуса
+		whereElRemove = whereElRemove.slice(1); // whereElRemove = .header-nav
 	}
+	//----------------------------------------------------------------------------
+
+	//----------------------------------------------------------------------------
+	// Удаление первого символа у строки
+	// Переменной backElRemove присваиваем значение параметра backEl
+	let backElRemove = backEl;
+	// Пишем условие если у переменной backElRemove первый символ равен -
+	if (backElRemove[0] == "-" || backElRemove[0] == "+") {
+		// то удаляем первый символ и присваиваем переменной backElRemove значение без минуса
+		backElRemove = backElRemove.slice(1);
+	}
+	//----------------------------------------------------------------------------
+
+
+	//----------------------------------------------------------------------------
+	// Переменные
+	let movingElement = document.querySelector(`${movEl}`);
+	let where = document.querySelector(`${whereElRemove}`); // whereEl = -.header-nav
+	//----------------------------------------------------------------------------
+
+
+	//----------------------------------------------------------------------------
+	// Вызов функции которая следит за шириной экрана
+	movingIn();
+	//----------------------------------------------------------------------------
+
+
+
+	//----------------------------------------------------------------------------
+	// Слежение за шириной экрана
 	window.addEventListener("resize", (e) => {
-		if(window.innerWidth <= width){
-			if(el[0] == '-'){
-				element.prepend(movingElement);
-			}
-			else{
-				element.append(movingElement);
-			}
-		}
-		else{
-			if(backEL[0] == '+') {
-				backElement.prepend(movingElement);
-			}else{
-				backElement.parentNode.insertBefore(movingElement, backElement.nextSibling);
-			}
-		}
+		movingIn();
 	});
+	//----------------------------------------------------------------------------
+
+
+
+	//----------------------------------------------------------------------------
+	// Функция которая отслеживает ширину экрана и говорит куда закинуть элемент вниз или вверх
+	function movingIn() {
+		// Если экран меньше 1140
+		if (window.innerWidth <= width) {
+			if (whereEl[0] == "-") {
+				// Переместить вверх элемента
+				where.prepend(movingElement);
+			} else {
+				// Переместить вниз элемента
+				where.append(movingElement);
+			}
+		} else {
+			//----------------------------------------------------------------------------
+			// Переменные
+			let backElement = document.querySelector(backElRemove);
+			let movingElem = document.querySelector(movEl);
+			//----------------------------------------------------------------------------
+
+			//----------------------------------------------------------------------------
+			// backEl = '-.header-nav'
+			if (backEl[0] == '-' || backEl[0] == '+') {
+				//where.prepend(movingElement);
+				if (backEl[0] == '-') {
+					backElement.prepend(movingElement);
+				} else {
+					backElement.append(movingElement);
+				}
+			} else {
+				//----------------------------------------------------------------------------
+				// Возвращение элемента обратно
+				// Перемещение после указанного элемента или внутрь 
+				backElement.parentNode.insertBefore(movingElem, backElement.nextSibling);
+				//----------------------------------------------------------------------------
+			}
+			//----------------------------------------------------------------------------
+		}
+	}
+	//----------------------------------------------------------------------------
+
+
 }
-
-
